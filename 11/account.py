277303cfb5903d2e470d4f5794a4ad25f334d399
Jared Claypoole <jared.claypoole@gmail.com>
2020-01-27 16:17:10 -0500

    Solution to pybite challenge 11 (including tests)

---
+++ 11/account.py
@@ -0,0 +1,51 @@
+class Account:
+
+    def __init__(self, name, start_balance=0):
+        self.name = name
+        self.start_balance = start_balance
+        self._transactions = []
+
+    @property
+    def balance(self):
+        return self.start_balance + sum(self._transactions)
+
+    # add dunder methods below
+
+    def __len__(self):
+        return len(self._transactions)
+
+
+    ## TODO -- make this much more concise with functools.total_ordering
+    def __gt__(self, account):
+        return self.balance > account.balance
+    def __lt__(self, account):
+        return self.balance < account.balance
+    def __eq__(self, account):
+        return self.balance == account.balance
+    def __ge__(self, account):
+        return self.balance >= account.balance
+    def __le__(self, account):
+        return self.balance <= account.balance
+
+
+    def __getitem__(self, index):
+        return self._transactions[index]
+
+    
+    def __iter__(self):
+        return iter(self._transactions)
+
+
+    ## TODO -- see whether there's a more concise way to do this
+    def __add__(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError("Expected amount added to be an integer, not {}".format(amount))
+        self._transactions.append(amount)
+    def __sub__(self, amount):
+        if not isinstance(amount, int):
+            raise ValueError("Expected amount subtracted to be an integer, not {}".format(amount))
+        self._transactions.append(-amount)
+
+
+    def __str__(self):
+        return "{} account - balance: {}".format(self.name, self.balance)
