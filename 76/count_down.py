6c39ec4e9a645bd2be7a58221187bcbd3553bf16
Jared Claypoole <jared.claypoole@gmail.com>
2021-04-29 17:41:59 -0400

    [76] Use stacked decorators for concision

--- 76/count_down.py
+++ 76/count_down.py
@@ -11,37 +11,19 @@ def _count_down(data: str):
     for i in range(len(data), 0, -1):
         print(data[:i])
 
+@count_down.register(int)
+@count_down.register(float)
 def _count_down_numeric(data: Union[int, float]):
     return count_down(str(data))
 
+@count_down.register(list)
+@count_down.register(tuple)
+@count_down.register(dict)
+@count_down.register(range)
 def _count_down_sequence(data: Sequence):
     return count_down("".join(str(x) for x in data))
 
 @count_down.register
-def _count_down(data: int):
-    return _count_down_numeric(data)
-
-@count_down.register
-def _count_down(data: float):
-    return _count_down_numeric(data)
-
-@count_down.register
-def _count_down(data: list):
-    return _count_down_sequence(data)
-
-@count_down.register
-def _count_down(data: tuple):
-    return _count_down_sequence(data)
-
-@count_down.register
-def _count_down(data: dict):
-    return count_down(list(data))
-
-@count_down.register
 def _count_down(data: set):
     return count_down(sorted(data))
-
-@count_down.register
-def _count_down(data: range):
-    return _count_down_sequence(data)
 
