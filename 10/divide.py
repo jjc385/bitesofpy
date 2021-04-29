177bcea9de4a2ff12a3f43002289acb5d29aab38
Jared Claypoole <jared.claypoole@gmail.com>
2021-04-29 17:10:12 -0400

    [10] Add my solution

--- 10/divide.py
+++ 10/divide.py
@@ -1,2 +1,9 @@
 def positive_divide(numerator, denominator):
-    pass
+    try:
+        result = numerator / denominator
+        if result < 0:
+            raise ValueError("Got a negative result")
+        return result
+    except ZeroDivisionError:
+        return 0
+
